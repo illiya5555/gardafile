c395394b173e6eb43dbd780018a92579de17c028
illiya5555 <106052285+illiya5555@users.noreply.github.com>
2025-06-19 13:42:24 +0300

    Updated EventsPage.tsx

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -289,7 +289,6 @@ const EventsPage = () => {
         )}
 
         {/* Gallery */}
-              {/* Image Modal */}
       {selectedImage !== null && (
         <div
           className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm"

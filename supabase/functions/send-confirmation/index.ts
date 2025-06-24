520f2cdf012e33b4161415f37d1698f6b30bb0cd
illiya5555 <106052285+illiya5555@users.noreply.github.com>
2025-06-24 22:03:02 +0300

    Updated index.ts

--- supabase/functions/send-confirmation/index.ts
+++ supabase/functions/send-confirmation/index.ts
@@ -238,6 +238,23 @@ serve(async (req) => {
             .footer { font-size: 12px; color: #666; text-align: center; margin-top: 30px; }
             .button { display: inline-block; background-color: #4B5563; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; }
           </style>
+          <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1040552124920921');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1040552124920921&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
         </head>
         <body>
           <div class="container">
